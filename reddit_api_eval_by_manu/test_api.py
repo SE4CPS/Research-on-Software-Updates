fb5360fd60a754527adfa42ad023c03cdd96798c
sberhe <solomon.berhe@gmail.com>
2025-11-26 17:24:17 -0800

    rename folders and refactor data

--- reddit_api_eval_by_manu/test_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'https://releasetrain.io/api/reddit'
-response = requests.get(url)
-
-print("Status Code:", response.status_code)
-print("Response Content:")
-print(response.text)
